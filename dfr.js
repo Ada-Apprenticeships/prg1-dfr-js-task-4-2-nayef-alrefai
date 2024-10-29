6cd68ea6cfde5899fd3921b3dc317569ee81b5fc
Nayef.Alrefai <159164377+nayef-alrefai@users.noreply.github.com>
2024-10-29 12:37:28 +0000

    added valid number function

--- dfr.js
+++ dfr.js
@@ -5,8 +5,27 @@ function fileExists(filename) {
 }
 
 function validNumber(value) {
+  if (typeof value === 'number') {
+    return !isNaN(value);
+  }
   
+  else if (typeof value === 'string') {
+    return /^-?\d+(\.\d+)?$/.test(value);
+  }
+
+  else return false;
 }
+//validation tests
+console.log(validNumber("0.1")); // → true
+console.log(validNumber("-1.12")); // → true
+console.log(validNumber(5)); // → true
+console.log(validNumber(-10)); // → true
+console.log(validNumber("5.")); // → false (decimal point must be followed by digits)
+console.log(validNumber("+5")); // → false (explicit plus sign not allowed)
+console.log(validNumber("0.0.1")); // → false (multiple decimal points)
+console.log(validNumber("three")); // → false (not a number)
+
+
 
 function dataDimensions(dataframe) {
 
